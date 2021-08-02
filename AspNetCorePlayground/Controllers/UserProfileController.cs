d293d1a14f2804c7868bc21d7097d8c231caad8f
Weida Zhao <weidazhao@MacBook-Pro.local>
2021-08-02 13:01:56 +0800

    init

---
+++ AspNetCorePlayground/Controllers/UserProfileController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AspNetCorePlayground.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserProfileController : ControllerBase
+    {
+        private readonly ILogger<UserProfileController> _logger;
+
+        public UserProfileController(ILogger<UserProfileController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<UserProfile> Get()
+        {
+            return new[]
+            {
+                new UserProfile { FirstName = "Weida", LastName = "Zhao" },
+            };
+        }
+    }
+
+    public record UserProfile
+    {
+        public string FirstName { get; init; }
+
+        public string LastName { get; init; }
+    }
+}
